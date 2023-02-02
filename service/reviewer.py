0f1d747a3eb1604c7c5a8274fd00f2b073fd0346
Carmenvic98 <carmenv.rios.cr@gmail.com>
2023-02-02 18:03:28 -0500

    servicios terminados

--- service/reviewer.py
+++ service/reviewer.py
@@ -17,3 +17,24 @@ class ReviewerService():
     def get_reviewer_by_name(self, name:str):
         result = self.db.query(ReviewerModel).filter(ReviewerModel.name == name).all()
         return result
+    
+    def create_reviewer(self, reviewer:Reviewer):
+        new_reviewer = ReviewerModel(
+            id= reviewer.id,
+            name= reviewer.name
+        )
+        self.db.add(new_reviewer)
+        self.db.commit()
+        return
+    
+    def update_reviewer(self, name:str, data:Reviewer):
+        reviewer.id = data.id
+        reviewer = self.db.query(ReviewerModel).filter(ReviewerModel.name == name).first()
+        self.db.commit()
+        return
+    
+    def delete_reviewer(self, id:int):
+        self.db.query(ReviewerModel).filter(ReviewerModel.id == id).delete()
+        self.db.commit()
+        return
+        
